1245129250620a2b9f772c2b56eee1f4cee80e26
Felipe Zani <59931479+FelipitoBr@users.noreply.github.com>
2024-09-05 10:00:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function download(bookName)
+{
+    if(bookName == "BookCoverReminder")
+    {
+        window.open("https://drive.google.com/file/d/1IOBAIeiig4n456cSNran1ve2W8oJrYyx/view?usp=sharing", '_blank').focus();
+    }else if (bookName == "BookCoverSeeds")
+    {
+        window.open("https://drive.google.com/file/d/1Bh39z0VUfqcxJjzsYyB1SwvybLHrrMhT/view?usp=sharing", '_blank').focus();
+    } else
+    {
+        window.open("https://drive.google.com/file/d/1GenM_7GAVLNGLDLtfUmgtFWHIxQQ4Xu9/view?usp=sharing","_blank").focus();
+    }
+
+}
